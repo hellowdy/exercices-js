a739e0a88e1c63848066a0f485db1381a1e01d4a
Elodie Mongellaz <mongellaz.e@gmail.com>
2021-10-14 14:02:25 +0200

    commit rewrite-if-else-question

--- 06-ifelse/6-rewrite-if-else-question/script.js
+++ 06-ifelse/6-rewrite-if-else-question/script.js
@@ -2,3 +2,7 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+ let message = (login == 'Employee') ? 'Hello' :
+      (login == 'Director') ? 'Greetings' :
+      (login == '') ? 'No login' :
+       '' ;
