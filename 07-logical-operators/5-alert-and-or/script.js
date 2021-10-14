d2bcca247b0f4f201ee75125e851e259e10bfe08
Elodie Mongellaz <mongellaz.e@gmail.com>
2021-10-14 15:43:38 +0200

    commit alert-and-or

--- 07-logical-operators/5-alert-and-or/script.js
+++ 07-logical-operators/5-alert-and-or/script.js
@@ -2,3 +2,4 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+alert( null || 2 && 3 || 4 ); /* => ( null || 3 || 4 ) => le résultat est 3. 
