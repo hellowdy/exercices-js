03947dc2f81375d8451352034fcd23b85dccaf68
Elodie Mongellaz <mongellaz.e@gmail.com>
2021-10-14 12:24:41 +0200

    commit check-standard

--- 06-ifelse/2-check-standard/script.js
+++ 06-ifelse/2-check-standard/script.js
@@ -2,3 +2,10 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+
+let nameJs = prompt('Quel est le nom "officiel" de Javascript');
+      if(nameJs == "ECMAScript"){
+            alert("Right!");
+      } else {
+            alert("Ne sais pas ? ECMAScript")
+      }
