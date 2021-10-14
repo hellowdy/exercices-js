a4f0a5fc95effce430c369e5c4f7a82df0808467
Elodie Mongellaz <mongellaz.e@gmail.com>
2021-10-14 11:03:49 +0200

    Commit 1 2 3

--- 01-variables/1-hello-variables/script.js
+++ 01-variables/1-hello-variables/script.js
@@ -2,3 +2,9 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+let admin;
+let userName;
+
+userName = "John";
+admin = userName ;
+alert(admin);
