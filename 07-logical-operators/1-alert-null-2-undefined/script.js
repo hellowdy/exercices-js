f7c5525e9103f4c80496a8057403fc93bb206a15
Elodie Mongellaz <mongellaz.e@gmail.com>
2021-10-14 14:06:15 +0200

    commit alert-null-2-underfined

--- 07-logical-operators/1-alert-null-2-undefined/script.js
+++ 07-logical-operators/1-alert-null-2-undefined/script.js
@@ -2,3 +2,4 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+alert( null || 2 || undefined ); // 2 est la première valeur vraie
