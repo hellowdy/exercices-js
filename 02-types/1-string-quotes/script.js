1fab96bbd9882fb3d6790fdfd646404ec3663bda
Elodie Mongellaz <mongellaz.e@gmail.com>
2021-10-14 11:31:27 +0200

    commit string-quotes

--- 02-types/1-string-quotes/script.js
+++ 02-types/1-string-quotes/script.js
@@ -2,3 +2,9 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+
+ alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); // hello Ilya
