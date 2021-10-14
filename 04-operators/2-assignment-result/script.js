2970c088583160dfaf404e03176a6f665036739e
Elodie Mongellaz <mongellaz.e@gmail.com>
2021-10-14 11:47:13 +0200

    commit assignment- result

--- 04-operators/2-assignment-result/script.js
+++ 04-operators/2-assignment-result/script.js
@@ -2,3 +2,6 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+let a = 2;
+
+let x = 1 + (a *= 2); // a = 4 ; x = 5
