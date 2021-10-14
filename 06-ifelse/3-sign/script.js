76684731e56efd1a256d445d428f837a5440602e
Elodie Mongellaz <mongellaz.e@gmail.com>
2021-10-14 13:32:46 +0200

    commit sign

--- 06-ifelse/3-sign/script.js
+++ 06-ifelse/3-sign/script.js
@@ -2,3 +2,11 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+let num = prompt('Type a number');
+if (num > 0){
+      alert(1);
+} else if (num < 0){
+      alert(-1);
+} else {
+      alert(0);
+}
