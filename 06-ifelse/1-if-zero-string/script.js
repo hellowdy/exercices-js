43973785245f049367f0a7b8f46662af391657eb
Elodie Mongellaz <mongellaz.e@gmail.com>
2021-10-14 12:13:19 +0200

    commit if-zero-string

--- 06-ifelse/1-if-zero-string/script.js
+++ 06-ifelse/1-if-zero-string/script.js
@@ -2,3 +2,6 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+if ("0") {
+      alert( 'Hello' ); // Oui `alert` sera affiché car la chaîne de caractère "0" est remplit.
+    }
