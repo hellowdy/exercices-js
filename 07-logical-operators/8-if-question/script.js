5cb8b24d79b2c6ca1ef9d0d5a6e89f50ef79707b
Elodie Mongellaz <mongellaz.e@gmail.com>
2021-10-15 09:39:51 +0200

    commit if-question

--- 07-logical-operators/8-if-question/script.js
+++ 07-logical-operators/8-if-question/script.js
@@ -2,3 +2,6 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+if (-1 || 0) alert( 'first' ); // résultat dans if : -1 => s'éxécute car vrai
+if (-1 && 0) alert( 'second' ); // résultat dans if : 0 => s'éxécute pas car faux
+if (null || -1 && 1) alert( 'third' ); // résultat dans if : 1 => S'éxécute car vrai
