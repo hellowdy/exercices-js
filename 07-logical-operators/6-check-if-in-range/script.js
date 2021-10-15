12fcf93c034bbafd438ce41daa85bf69c123e65e
Elodie Mongellaz <mongellaz.e@gmail.com>
2021-10-15 09:21:05 +0200

    commit check-if-in-range

--- 07-logical-operators/6-check-if-in-range/script.js
+++ 07-logical-operators/6-check-if-in-range/script.js
@@ -2,3 +2,5 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+if (age <= 90 && age >= 14) 
+
